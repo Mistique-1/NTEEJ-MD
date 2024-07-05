8b89008d5dae61956bfda53c1451b832432fd829
Mistique-1 <mose9679@gmail.com>
2024-07-06 00:28:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync(".env"))
   require("dotenv").config({ path: __dirname + "/.env" });
 
 //=======[dependencies]====================//
-global.SESSION_ID = process.env.SESSION_ID || "";
+global.SESSION_ID = process.env.SESSION_ID || "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0pCTldaTFlPMWdZTEdoN0lXK2U1S0NLTVhhNDBVM2NSTVZFTEVMb2hXcz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoibWh1ZXFBZDQxMGxnZjcyek5FY0ljRDlFQitQMENxdDNnV1orZ3N5U2VHQT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJnSHZTTm1vb2pKV1Nyc1dPcXFTZ1pDMDBMWVRwb2lMaWsxUEU4alBoeEg4PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJVa3Y3bys4Znd5b0hzYjlvUmNFU3pTdmlOVnhnVzdPdXJkZTQ1bWZQOXhZPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImFJaGdoVjl0NWs5L1orc0YrMFFkZWEzMllwQnFsTDBGOEoxY2NKbXlNbVE9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImpWYm1tODYza054ZFZySU5aZEM5cG0ydVdxQnM4cnpZU01FVGRtNWw0VTQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiK0JGRktQOTFCWU84S0pnbU5DSmpSNjhhRzJrOXpNWFdTZTBkQ1pGdDgxQT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTm1UeEs3dCtoZ1IwM1l5ZFIybE9DTEcwK2hCRlBydlJSVkF4Ry8rNkJGYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImU2ZDVXL1NQaGhWMk1zQnBwR1JKdUdCa1Q1VTNRTEFKYU1LM2lCZGszS1NEOStnb3hLM1p5M21HS3RDRXBPSjkzRVRKVENvVGhCaitGT2hjZ25qaUJBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTA3LCJhZHZTZWNyZXRLZXkiOiJxUVQ3d0doN09jdVhjalgrUEkySnhNWGJtUmxORGdTNzZVNDBQTVBvbEw4PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJob3pycDA5WFFEbUhJVGVFUnVzaTF3IiwicGhvbmVJZCI6ImZlNjRhMzdlLThjYmQtNDJiNC1hYmU4LWM0NzA3MTZiZjAzYyIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5ZCtyOEVvTmJobGlqN21DVnRkclFxNGRzcU09In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZ1Y3RkJLMFpkdEI1bm1sZ0JqVFhQTVpzYXpzPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IlNCMUdZRTdYIiwibWUiOnsiaWQiOiIyNTQ3Njk4OTQxMTg6MjFAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ0t5di9Ob0RFT0xHb2JRR0dBTWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6IlZOSGFRL0dpMkgwUm8xRHdlV1djMHFxNGNGb0hLbGMwVTFuSXBkakNtWEE9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkhlVDRCMzBpSHlPdERYNGF4d21Sc1QwT3kvdlFvODdLNXdTSVUyQ3FObHVUSHlERWFjUTNUY0hYNWJkaE9zRVJwU2ZVUGdra1F4NEFVK2drdStDZUNBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJHcUhCcnpidVdlcDFDbUQ5NDRhcFNTUk9raWtWVUpoYTd4bEkrNVZwaGdMWmpXY1h2ZUZLUjZWTk0zZDArOUxCdlZ6VmpFbGVGNzl5ZTFQNjkzMzhDQT09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjI1NDc2OTg5NDExODoyMUBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJWVFIya1B4b3RoOUVhTlE4SGxsbk5LcXVIQmFCeXBYTkZOWnlLWFl3cGx3In19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIwMjE0MzgzfQ==";
 global.MONGODB = process.env.MONGODB_URI || "";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.sudo = process.env.SUDO
@@ -12,7 +12,7 @@ global.sudo = process.env.SUDO
   : "null";
 global.owner = process.env.OWNER_NUMBER
   ? process.env.OWNER_NUMBER.replace(/[\s+]/g, "")
-  : "263714497545";
+  : "254769894118";
 global.THUMB_IMAGE =
   process.env.THUMB_IMAGE ||
   process.env.IMAGE ||
@@ -27,11 +27,11 @@ module.exports = {
   HANDLERS: process.env.PREFIX || ".",
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
-  caption: process.env.CAPTION || "`NTEEJ-MD`",
-  author: process.env.PACK_AUTHER || "NTEEJ-MD",
-  packname: process.env.PACK_NAME || "NTEEJ",
-  botname: process.env.BOT_NAME || "NTEEJ-MD",
-  ownername: process.env.OWNER_NAME || "Nteej",
+  caption: process.env.CAPTION || "`Mozz`",
+  author: process.env.PACK_AUTHER || "Mozz",
+  packname: process.env.PACK_NAME || "Mose_Tonnex",
+  botname: process.env.BOT_NAME || "Kamdudu Md",
+  ownername: process.env.OWNER_NAME || "Mose_Tonex",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
@@ -68,7 +68,7 @@ global.waPresence = process.env.WAPRESENCE || "null";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null";
-global.read_status = process.env.AUTO_READ_STATUS || "false";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "null";
 global.read_status_from = process.env.READ_STATUS_FROM || "null";
